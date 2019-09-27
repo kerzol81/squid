f5fa259cd8867ca7d72e30581cbb547306a8b6b9
Z <kerenyi.nagy.zoltan@gmail.com>
2019-09-27 10:16:52 +0200

    Create nmea.py

---
+++ nmea.py
@@ -0,0 +1,32 @@
+class Nmea:
+    def __init__(self, coordinate, direction):
+        self.coordinate = coordinate
+        self.direction = direction
+
+    def convert_to_decimal(self):
+        try:
+            """ NMEA to Decimal Degrees"""
+            if self.coordinate.find('.') is 5:
+                """ longitude in the DDDMM.MMMMM format """
+                dd = int(float(self.coordinate[:3].strip('0')))
+                ss = float(self.coordinate) - dd * 100
+                if self.direction == 'E':
+                    return round(dd + (ss / 60), 6)
+                elif self.direction == 'W':
+                    return round(dd + (ss / 60), 6) * -1
+                else:
+                    return '00.0000000'
+            elif self.coordinate.find('.') is 4:
+                """  latitude in the DDMM.MMMMM format """
+                dd = int(float(self.coordinate) / 100)
+                ss = float(self.coordinate) - dd * 100
+                if self.direction == 'N':
+                    return dd + (ss / 60)
+                elif self.direction == 'S':
+                    return dd + (ss / 60) * -1
+                else:
+                    return '00.000000'
+            else:
+                return '00.000000'
+        except:
+            return '00.00000'
