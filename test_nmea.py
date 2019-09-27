bd4452f7dc10fd3f294d16b0a3faf465c93ed6c0
Z <kerenyi.nagy.zoltan@gmail.com>
2019-09-27 12:50:41 +0200

    Create test_nmea.py

---
+++ test_nmea.py
@@ -0,0 +1,20 @@
+import unittest
+from nmea import *
+
+
+class TestNmeaConverter(unittest.TestCase):
+    def test_valid_lat_1(self):
+        n = Nmea('5132.0000', 'N')
+        self.assertAlmostEqual(n.convert_to_decimal(), 51.533333)
+
+    def test_valid_lat_2(self):
+        n = Nmea('5132.0000', 'S')
+        self.assertAlmostEqual(n.convert_to_decimal(), -51.533333)
+
+    def test_valid_lon_1(self):
+        n = Nmea('01323.629', 'E')
+        self.assertAlmostEqual(n.convert_to_decimal(), 13.393817)
+
+    def test_valid_lon_1(self):
+        n = Nmea('01323.629', 'W')
+        self.assertAlmostEqual(n.convert_to_decimal(), -13.393817)
