5057e74660900eaf3e9ad8df2fe003a0c05473db
Z <kerenyi.nagy.zoltan@gmail.com>
2019-09-27 13:18:19 +0200

    Update test_nmea.py

--- test_nmea.py
+++ test_nmea.py
@@ -4,19 +4,13 @@ from nmea import *
 
 class TestNmeaConverter(unittest.TestCase):
     def test_valid_lat_1(self):
-        n = Nmea('5132.0000', 'N')
-        self.assertAlmostEqual(n.convert_to_decimal(), 51.533333)
+        self.assertAlmostEqual(Nmea('5132.0000', 'N').convert_to_decimal(), 51.533333)
 
     def test_valid_lat_2(self):
-        n = Nmea('5132.0000', 'S')
-        self.assertAlmostEqual(n.convert_to_decimal(), -51.533333)
+        self.assertAlmostEqual(Nmea('5132.0000', 'S').convert_to_decimal(), -51.533333)
 
     def test_valid_lon_1(self):
-        n = Nmea('01323.629', 'E')
-        self.assertAlmostEqual(n.convert_to_decimal(), 13.393817)
+        self.assertAlmostEqual(Nmea('01323.629', 'E').convert_to_decimal(), 13.393817)
 
     def test_valid_lon_2(self):
-        n = Nmea('01323.629', 'W')
-        self.assertAlmostEqual(n.convert_to_decimal(), -13.393817)
-        
-        
+        self.assertAlmostEqual(Nmea('01323.629', 'W').convert_to_decimal(), -13.393817)
