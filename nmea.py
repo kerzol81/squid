4367ddd0be702afba22ee66ffb48d06d9e3a00d8
Z <kerenyi.nagy.zoltan@gmail.com>
2019-09-27 11:13:46 +0200

    Update nmea.py

--- nmea.py
+++ nmea.py
@@ -3,7 +3,9 @@ class Nmea:
         self.coordinate = coordinate
         self.direction = direction
 
+    @property
     def convert_to_decimal(self):
+        zero = '00.00000'
         try:
             """ NMEA to Decimal Degrees"""
             if self.coordinate.find('.') is 5:
@@ -11,22 +13,22 @@ class Nmea:
                 dd = int(float(self.coordinate[:3].strip('0')))
                 ss = float(self.coordinate) - dd * 100
                 if self.direction == 'E':
-                    return round(dd + (ss / 60), 6)
+                    result = str(round(dd + (ss / 60), 6))
                 elif self.direction == 'W':
-                    return round(dd + (ss / 60, 6) * -1, 6)
+                    result = str(round(dd + (ss / 60, 6), 6) * -1)
                 else:
-                    return '00.0000000'
-            elif self.coordinate.find('.') is 4:
+                    result = zero
+                return result
+            if self.coordinate.find('.') is 4:
                 """  latitude in the DDMM.MMMMM format """
                 dd = int(float(self.coordinate) / 100)
                 ss = float(self.coordinate) - dd * 100
                 if self.direction == 'N':
-                    return round(dd + (ss / 60), 6)
+                    result = str(round(dd + (ss / 60), 6))
                 elif self.direction == 'S':
-                    return round(dd + (ss / 60) * -1, 6)
+                    result = str(round(dd + (ss / 60), 6) * -1)
                 else:
-                    return '00.000000'
-            else:
-                return '00.000000'
+                    result = zero
+                return result
         except:
-            return '00.00000'
+            return zero
