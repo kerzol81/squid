2e47876a14127970b4d674fac4afb4874bc07282
Z <kerenyi.nagy.zoltan@gmail.com>
2019-09-30 11:22:06 +0200

    Update nmea.py

--- nmea.py
+++ nmea.py
@@ -5,26 +5,35 @@ class Nmea:
 
     """ NMEA to Decimal Degrees"""
     def convert_to_decimal(self):
-        try:
-            if self.coordinate.find('.') is 5:
-                """ longitude in the DDDMM.MMMMM format """
-                dd = int(float(self.coordinate[:3].strip('0')))
-                ss = float(self.coordinate) - dd * 100
-                if self.direction == 'E':
-                    return round(dd + (ss / 60), 6)
-                elif self.direction == 'W':
-                    return round(dd + (ss / 60), 6) * -1
-                else:
-                    return 0.0
-            if self.coordinate.find('.') is 4:
-                """  latitude in the DDMM.MMMMM format """
-                dd = int(float(self.coordinate) / 100)
-                ss = float(self.coordinate) - dd * 100
-                if self.direction == 'N':
-                    return round(dd + (ss / 60), 6)
-                elif self.direction == 'S':
-                    return round(dd + (ss / 60), 6) * -1
-                else:
-                    return 0.0
-        except:
+        if not isinstance(self.coordinate, str) or not len(self.coordinate) is 9:
+            raise TypeError('invalid coordinate')
+        if not self.coordinate.find('.'):
+            raise TypeError('invalid coordinate, missing .')
+        if not self.coordinate.find('.') in [4, 5]:
+            raise TypeError('invalid coordinate, point is not in proper position')
+        if self.direction not in ['N', 'S', 'W', 'E']:
+            raise TypeError('only N, S, E or W are valid directions')
+
+        if self.coordinate.find('.') is 5:
+            """ longitude in the DDDMM.MMMMM format """
+            dd = int(float(self.coordinate[:3].strip('0')))
+            ss = float(self.coordinate) - dd * 100
+            if self.direction == 'E':
+                return round(dd + (ss / 60), 6)
+            elif self.direction == 'W':
+                return round(dd + (ss / 60), 6) * -1
+            else:
+                return 0.0
+
+        elif self.coordinate.find('.') is 4:
+            """  latitude in the DDMM.MMMMM format """
+            dd = int(float(self.coordinate) / 100)
+            ss = float(self.coordinate) - dd * 100
+            if self.direction == 'N':
+                return round(dd + (ss / 60), 6)
+            elif self.direction == 'S':
+                return round(dd + (ss / 60), 6) * -1
+            else:
+                return 0.0
+        else:
             return 0.0
